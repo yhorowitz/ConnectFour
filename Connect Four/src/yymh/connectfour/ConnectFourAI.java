04b7249c46044282b289a65a486cc6c1a1e43016
yitzih <yitzchakhorowitz@gmail.com>
2016-01-18 03:39:28 -0500

    began change of heuristic used in AI decisions

--- Connect Four/src/yymh/connectfour/ConnectFourAI.java
+++ Connect Four/src/yymh/connectfour/ConnectFourAI.java
@@ -5,8 +5,8 @@ import java.util.Random;
 
 public class ConnectFourAI {
 
-	static final int DEFAULT_BEGINNER_MOVE_DEPTH = 2;
-	static final int DEFAULT_INTERMEDIATE_MOVE_DEPTH = 4;
+	static final int DEFAULT_BEGINNER_MOVE_DEPTH = 3;
+	static final int DEFAULT_INTERMEDIATE_MOVE_DEPTH = 5;
 	static final int DEFAULT_EXPERT_MOVE_DEPTH = 7;
 	static final int DEFAULT_NO_AI_MOVE_DEPTH = 0;
 
@@ -97,7 +97,7 @@ public class ConnectFourAI {
 		int maxScore = Integer.MIN_VALUE;
 		int column = -1;
 		
-		for (int i = 1; i < columnScores.size(); i++) {
+		for (int i = 0; i < columnScores.size(); i++) {
 			if (columnScores.get(i) > maxScore)
 				maxScore = columnScores.get(i);
 		}
@@ -109,8 +109,10 @@ public class ConnectFourAI {
 		Random rand = new Random();
 		while (true) {
 			column = rand.nextInt(7);
-			  	
+
 			if (columnScores.get(column) == maxScore) {
+				//System.out.println("max score is: " + maxScore);
+				//System.out.println("returning column #: " + column);
 				return column;
 			}
 		}
@@ -163,7 +165,7 @@ public class ConnectFourAI {
 		
 		// depthDiff is used to make wins/losses performed at a later depth less valuable 
 		// so that there is no delay if all moves lead to a terminal condition
-		int depthDiff = 1000 - (depth * 10);
+		int depthDiff = (this.moveDepth - depth);
 		
 		//get score for each column 
 		for (int i = 0; i < ConnectFour.NUM_OF_COLUMNS; i++)
@@ -178,9 +180,6 @@ public class ConnectFourAI {
 			{
 				//simulate move in copied game
 				gameClone.makeMove(i, false);
-				
-				//TODO ADD TO LOGGING
-				//ConnectFourConsoleDriver.printCurrentBoard(gameClone.getCurrentGameState());
 			
 				//if the result is a win condition
 				if (gameClone.checkForWin(gameClone.getCurrentPlayer()))
@@ -188,7 +187,7 @@ public class ConnectFourAI {
 					//add Integer.MAX_VALUE or Integer.MIN_VALUE respectively based on whose turn it is
 					scoreToAdd = (game.isAITurn() ? Integer.MAX_VALUE - depthDiff : Integer.MIN_VALUE + depthDiff);
 				}
-				else if (depth != 0) //if not a win condition and not at the lowest depth, branch to a lower depth
+				else if (depth > 1) //if not a win condition and not at the lowest depth, branch to a lower depth
 				{
 					gameClone.switchPlayer();
 					ArrayList<Integer> futureScores = (minimax(depth - 1, gameClone));
@@ -199,28 +198,26 @@ public class ConnectFourAI {
 					else {
 						scoreToAdd = getHighestScore(futureScores);
 					}
+					
 
 				}
 				else {
-	
-					if (ConnectFour.checkForThreeInARow(game.getCurrentPlayer(), i, gameClone))
-						scoreToAdd = 10000;
-					else 
-						scoreToAdd = 0;
+					//score the state of the board
+					scoreToAdd = scoreBoardState(gameClone);
 									
-					scoreToAdd += depthDiff;
+					//scoreToAdd += depthDiff;
 					
 					if (!game.isAITurn()) //if it is the players turn make it a negative number
 						scoreToAdd *= -1;
-					}
+				}
 			}
 			else //if move isnt valid return the worst possible value so this column doesnt get chosen
 			{
 				scoreToAdd = ((game.isAITurn()) ? Integer.MIN_VALUE + depthDiff : Integer.MAX_VALUE - depthDiff);
 			}
 			
-			//TODO ADD LOGGING 
-			System.out.println("adding Score: " + scoreToAdd + " for column: " + i + " at depth: " + depth);
+			//TODO ADD LOGGING (maybe)
+			//System.out.println("adding Score: " + scoreToAdd + " for column: " + i + " at depth: " + depth);
 			
 			//add the score for the current column to the list
 			scores.add(scoreToAdd);
@@ -232,4 +229,147 @@ public class ConnectFourAI {
 		return scores;
 
 	}
+	
+	public int scoreBoardState(ConnectFour game){
+		int totalScore = 0;
+		int aiScore = 0;
+		int playerScore = 0;
+		int[][] boardState = game.getCurrentGameState();
+		
+		/*
+		 * SCORING (for MAX, MIN is score for MAX * -1):
+		 * 
+		 * WIN CONDITION = 250,000
+		 * ---------------------
+		 * horizontal & diagonal
+		 * ---------------------
+		 * 3 IAR + 1 OS each side = 5,000
+		 * 3 IAR + 1 OS one side = 200
+		 * 3 IAR + 0 OS = 0
+		 * 
+		 * 2 IAR + 2 OS each side = 1000
+		 * 2 IAR + 1 OS each side = 600
+		 * 2 IAR + 2 OS one side = 600
+		 * 2 IAR + 1 or less OS = 0
+		 * 
+		 * 1 IAR + 2 OS each side = 300
+		 * 1 IAR + 3 OS any sides = 50
+		 * 
+		 * -----------
+		 * vertical
+		 * -----------
+		 * 3 IAR = 500
+		 * 2 IAR = 100
+		 */
+		
+		//SCORES
+		final int SCORE_THREE_IAR_TWO_OS = 5000;
+		final int SCORE_THREE_IAR_ONE_OS = 2000;
+		
+		final int SCORE_TWO_IAR_FOUR_OS = 1000;
+		final int SCORE_TWO_IAR_THREE_OS = 800;
+		final int SCORE_TWO_IAR_TWO_OS = 600;
+
+		final int SCORE_ONE_IAR_FOUR_OS = 300;
+		final int SCORE_ONE_IAR_THREE_OS = 50;
+		
+		final int SCORE_VERT_THREE_IAR = 500;
+		final int SCORE_VERT_TWO_IAR = 100;
+
+		
+		final int EM = 0;
+		final int AI = game.isAITurn() ? game.getCurrentPlayer() : game.getHumanPlayerNumber();
+		final int PL = game.getHumanPlayerNumber();
+		
+		//calculate score for current state of the board in each direction
+		
+		/*
+		 * (horizontal)
+		 * check for 3 IAR + 1 OS each side 
+		 * or for 3 IAR + 1 OS on one side
+		 */
+		for (int i = 0; i < boardState.length; i++) {
+			for (int j = 0; j < boardState[i].length - 4; j++) {
+				
+				//look for 3IAR+1OS each side for AI (only if there is enough room left on the row for it to be possible)
+				if (j < boardState[i].length - 5 &&
+					(boardState[i][j] == EM && boardState[i][j + 1] == AI && boardState[i][j + 2] == AI && boardState[i][j + 3] == AI && boardState[i][j + 4] == EM)) {
+						aiScore += SCORE_THREE_IAR_TWO_OS;
+				}
+				//look for 3IAR+1OS before
+				else if (boardState[i][j] == EM && boardState[i][j + 1] == AI && boardState[i][j + 2] == AI && boardState[i][j + 3] == AI) {
+					aiScore += SCORE_THREE_IAR_ONE_OS;
+				}
+				//look for 3IAR+1OS after
+				else if (boardState[i][j] == AI && boardState[i][j + 1] == AI && boardState[i][j + 2] == AI && boardState[i][j + 3] == EM) {
+					aiScore += SCORE_THREE_IAR_ONE_OS;
+				}
+					
+			}
+		}
+		
+		//check for 2 or 3 in a row vertically
+		for (int i = 0; i < boardState[0].length; i++) {
+			for (int j = 0; j < boardState.length - 3; j++) {
+				
+				//check for 3 in a row
+				if (boardState[j][i] == EM && boardState[j + 1][i] == AI && boardState[j + 2][i] == AI && boardState[j + 3][i] == AI) {
+					aiScore += SCORE_VERT_THREE_IAR;
+				}	
+				//check for 2 in a row
+				else if (boardState[j][i] == EM && boardState[j + 1][i] == EM && boardState[j + 2][i] == AI && boardState[j + 3][i] == AI) {
+					aiScore += SCORE_VERT_TWO_IAR;
+				}
+			}
+		}
+		
+		//diagonal upper left -> bottom right
+		for (int i = 0; i < boardState.length - 4; i++) {
+			for (int j = 0; j < boardState[i].length - 4; j++) {
+				
+				//look for 3IAR+1OS each side for AI (only if there is enough room left on the row for it to be possible)
+				if (i < boardState.length - 5 && j < boardState[i].length - 5 && 
+					(boardState[i][j] == EM && boardState[i + 1][j + 1] == AI && boardState[i + 2][j + 2] == AI && boardState[i + 3][j + 3] == AI && boardState[i + 4][j + 4] == EM)) {
+						aiScore += SCORE_THREE_IAR_TWO_OS;
+					
+				}
+				//look for 3IAR+1OS before
+				else if (boardState[i][j] == EM && boardState[i + 1][j + 1] == AI && boardState[i + 2][j + 2] == AI && boardState[i + 3][j + 3] == AI) {
+					aiScore += SCORE_THREE_IAR_ONE_OS;
+				}
+				//look for 3IAR+1OS after
+				else if (boardState[i][j] == AI && boardState[i + 1][j + 1] == AI && boardState[i + 2][j + 2] == AI && boardState[i + 3][j + 3] == EM) {
+					aiScore += SCORE_THREE_IAR_ONE_OS;
+				}
+				
+			}
+		}
+		
+		//diagonal upper right -> bottom left
+		for (int i = 0; i < boardState.length - 4; i++) {
+			for (int j = 3; j < boardState[i].length; j++) {
+				
+				//check for how many in a row. if found skip past it (dont double count)
+				//check if it there are open spaces around it
+				if (i < boardState.length && j > 3 &&
+					(boardState[i][j] == EM && boardState[i + 1][j - 1] == AI && boardState[i + 2][j - 2] == AI && boardState[i + 3][j - 3] == AI && boardState[i + 4][j - 4] == EM)) {
+						aiScore += SCORE_THREE_IAR_TWO_OS;
+				}
+				//look for 3IAR+1OS before
+				else if (boardState[i][j] == EM && boardState[i + 1][j - 1] == AI && boardState[i + 2][j - 2] == AI && boardState[i + 3][j - 3] == AI) {
+					aiScore += SCORE_THREE_IAR_ONE_OS;
+				}
+				//look for 3IAR+1OS after
+				else if (boardState[i][j] == AI && boardState[i + 1][j - 1] == AI && boardState[i + 2][j - 2] == AI && boardState[i + 3][j - 3] == EM) {
+					aiScore += SCORE_THREE_IAR_ONE_OS;
+				}
+			}
+		}
+		
+		//calculate disjointed 3IARs
+		//ConnectFourConsoleDriver.printCurrentBoard(boardState);
+		//System.out.println("Calculated Score: " + aiScore);
+		return aiScore;
+	}
+	
 }
